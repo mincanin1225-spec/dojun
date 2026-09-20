fb2917903496be758aca8954fb1d51322844f559
mincanin1225-spec <mincanin1225@gmail.com>
2026-09-21 08:02:32 +0900

    v66: cache stock-number prep files

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='dojun-pwa-v65-shoppingraw1';
+const CACHE='dojun-pwa-v66-prepstock1';
 const CORE=[
-  './meal-stock-v63.js','./meal-workflow-v65.js','./legacy-inventory-v61-photo.js',
+  './meal-stock-v66.js','./meal-workflow-v66.js','./legacy-inventory-v61-photo.js',
   './','./index.html','./manifest.webmanifest','./legacy-v24.html',
   './icons/icon-192.svg','./icons/icon-512.svg',
   './legacy-management-v29.js','./legacy-management-v30.js','./legacy-management-v31.js',
