93cf6cec06626c1d80d4b174cccbe3298e0f1dea
mincanin1225-spec <mincanin1225@gmail.com>
2026-09-21 16:08:30 +0900

    v70: refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='dojun-pwa-v69-finalux1';
+const CACHE='dojun-pwa-v70-finalaudit1';
 const CORE=[
-  './meal-stock-v66.js','./meal-workflow-v69.js','./legacy-inventory-v61-photo.js',
-  './','./index.html','./manifest.webmanifest','./legacy-v69.html',
+  './meal-stock-v66.js','./meal-workflow-v70.js','./legacy-inventory-v61-photo.js',
+  './','./index.html','./manifest.webmanifest','./legacy-v70.html',
   './icons/icon-192.svg','./icons/icon-512.svg',
   './legacy-management-v29.js','./legacy-management-v30.js','./legacy-management-v31.js',
   './legacy-management-v32.js','./legacy-management-v32-1.js','./legacy-management-v33.js',
