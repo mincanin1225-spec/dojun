ac60b8fbaa1bed9d9e95dd172e39ad9d5468ab8d
mincanin1225-spec <mincanin1225@gmail.com>
2026-09-21 11:41:13 +0900

    v68 hotfix: refresh cache for scroll-restoring shell

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='dojun-pwa-v68-portionback2';
+const CACHE='dojun-pwa-v68-scroll3';
 const CORE=[
   './meal-stock-v66.js','./meal-workflow-v68-portion2.js','./legacy-inventory-v61-photo.js',
-  './','./index.html','./manifest.webmanifest','./legacy-v68-nav2.html',
+  './','./index.html','./manifest.webmanifest','./legacy-v68-scroll3.html',
   './icons/icon-192.svg','./icons/icon-512.svg',
   './legacy-management-v29.js','./legacy-management-v30.js','./legacy-management-v31.js',
   './legacy-management-v32.js','./legacy-management-v32-1.js','./legacy-management-v33.js',
