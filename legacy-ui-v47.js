bab3b6fe8941f3bd596a703336e30c52983a603f
mincanin1225-spec <mincanin1225@gmail.com>
2026-09-21 12:25:49 +0900

    v69: align fallback version

--- legacy-ui-v47.js
+++ legacy-ui-v47.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
   const APP_NAME=window.__DOJUN_RELEASE?.name||'도준이키우기';
-  const DISPLAY_VER=window.__DOJUN_RELEASE?.version||'v68';
+  const DISPLAY_VER=window.__DOJUN_RELEASE?.version||'v69';
 
   function patchBrand(){
     if(document.title!==APP_NAME)document.title=APP_NAME;
