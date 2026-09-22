c2201c9eb9a4b6e8c49c1d6ba80d78ba7f24c5ca
mincanin1225-spec <mincanin1225@gmail.com>
2026-09-22 09:29:14 +0900

    v70: refresh prep progress cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dojun-pwa-v70-manualaudit2';
+const CACHE='dojun-pwa-v70-prepprogress1';
 const CORE=[
   './meal-stock-v66.js','./meal-workflow-v70.js','./legacy-inventory-v61-photo.js',
   './','./index.html','./manifest.webmanifest','./legacy-v70.html',
