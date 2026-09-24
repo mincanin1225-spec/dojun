297bede5e981dace8e4b25059094de5ad3d6c5f5
mincanin1225-spec <mincanin1225@gmail.com>
2026-09-24 22:47:33 +0900

    test: 재고 충분 상태 새 문구 반영

--- tests/v70-simple-make-flow.cjs
+++ tests/v70-simple-make-flow.cjs
@@ -48,7 +48,7 @@ assert.equal(made.remainingCount,1);
 assert(ctx.inventory.chicken.qty<100,'making must deduct raw ingredients');
 
 html=ctx.vShop();
-assert(html.includes('필요량 준비됨'));
+assert(html.includes('기존 재고로 충분'));
 assert(html.includes('완료 취소'),'checked make task must behave like a reversible checklist while untouched');
 const undoKey=[...html.matchAll(/data-v71-makecheck="([^"]+)"/g)].map(x=>x[1]).find(x=>decodeURIComponent(x).endsWith('|닭고기'));assert(undoKey);
 clickMake(undoKey);
